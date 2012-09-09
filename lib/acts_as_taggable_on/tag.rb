d735144e56daca085cf7efd79806252eeb816d6f
Andy Chen <ruyanto@gmail.com>
2012-09-09 14:53:39 +0800

    Change tag name max length to 50

--- lib/acts_as_taggable_on/tag.rb
+++ lib/acts_as_taggable_on/tag.rb
@@ -12,7 +12,7 @@ module ActsAsTaggableOn
 
     validates_presence_of :name
     validates_uniqueness_of :name
-    validates_length_of :name, :maximum => 255
+    validates_length_of :name, :maximum => 50 #255
 
     ### SCOPES:
 
